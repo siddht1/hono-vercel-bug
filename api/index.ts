13ab6bc20b1d98508cb14664dd6fab76b6b2011c
siddht1 <rt1yrru+github@gmail.com>
2024-06-13 02:05:51 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,10 @@
 import { Hono } from 'hono';
-// import { handle } from '@hono/node-server/vercel';
+import { handle } from '@hono/node-server/vercel';
 
 const app = new Hono().basePath('/api');
 
 app.get('/', (c) => {
-  return c.json({'Hello from Hono!'});
+  return c.json({app:'l1'});
 });
 
 app.post('/', async (c) => {
