98736f14c4cd1982c446e5e30093d7948c37fa46
siddht1 <rt1yrru+github@gmail.com>
2024-06-13 09:46:32 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -5,22 +5,18 @@ const app = new Hono().basePath('/api');
 
 app.get('/', (c) => {
   // Retrieve IP address and user-agent
-  const ip = c.ip;
+
   const userAgent = c.req.headers.get('user-agent'); // Access user-agent header
 console.log(c);
   // Prepare response data
   const response = {
     message: 'Hello from Hono!',
-   IP: ip,
-    IP_2: c.req.headers.get('x-real-ip'),
-    IP_3: c.req.headers.get('x-forwarded-for'),
-   IP_4: c.req.headers.get('x-vercel-forwarded-for'),
-
-        latitude: c.req.headers.get('x-vercel-ip-latitude'),
-    longitude: c.req.headers.get('x-vercel-ip-longitude'),
-    city: c.req.headers.get('x-vercel-ip-city'),
-    region: c.req.headers.get('x-vercel-ip-country-region'),
-    country: c.req.headers.get('x-vercel-ip-country'),
+   IP: c.ip|| c.req.headers.get('x-real-ip')||c.req.headers.get('x-forwarded-for')||c.req.headers.get('x-vercel-forwarded-for'),
+    VERCEL_latitude: c.req.headers.get('x-vercel-ip-latitude'),
+    VERCEL_longitude: c.req.headers.get('x-vercel-ip-longitude'),
+    VERCEL_city: c.req.headers.get('x-vercel-ip-city'),
+    VERCEL_region: c.req.headers.get('x-vercel-ip-country-region'),
+   VERCEL_country: c.req.headers.get('x-vercel-ip-country'),
    UA: userAgent,
     date_time: new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" }),
   };
